6cf28bee9249156298e42ee5c0dbf251bed08175
sumon paul <sumpaul100@gmail.com>
2023-05-04 23:55:00 +0600

    theme different color seted

--- src/App.js
+++ src/App.js
@@ -13,8 +13,18 @@ import { ThemeProvider } from "styled-components";
 function App() {
   const theme = {
     colors: {
-      bg: "#000",
-      color: "white",
+      bg: "#f6f8fa",
+      footer_bg: "#0a1435",
+      btn: "rgb(98 84 243)",
+      border: "rgba(98 ,84, 243, .5)",
+      hr: "#ffffff",
+      gradient: "linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252) 100%)",
+      shadow: "rgba(0,0,0,0.2) 0px 1px 3px 0px, rgba(27, 31, 35, .15) 0px 0px 0px 1px",
+      shadowSupport: "rgba(0,0,0,0.16) 0px 1px 4px",
+    },
+    media: {
+      mobile: "768px",
+      tab: "998px",
     },
   };
   return (
