4c122a5cf6c3d2ccab9be2675d94811b60338775
sumon paul <sumpaul100@gmail.com>
2023-05-06 11:44:37 +0600

    servces section start

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
+import Services from "./components/Services";
 const Home = () => {
   const Data = {
     name: "Super Shop",
   };
-  return <HeroSection myData={Data} />;
+  return (
+    <>
+      <HeroSection myData={Data} />
+      <Services />
+    </>
+  );
 };
 
 export default Home;
