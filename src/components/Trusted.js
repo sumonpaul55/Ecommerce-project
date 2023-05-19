1b514db417ebcf4559fa351d27a053764f1d7dd2
sumon paul <sumpaul100@gmail.com>
2023-05-19 23:44:28 +0600

    trusted page bg added

--- src/components/Trusted.js
+++ src/components/Trusted.js
@@ -1,7 +1,11 @@
 import React from "react";
+import styled from "styled-components";
 
 const Trusted = () => {
-  return <div>Trusted</div>;
+  return <Wrapper>Trusted</Wrapper>;
 };
-
+const Wrapper = styled.section`
+  padding: 9rem 0;
+  background-color: ${({ theme }) => theme.colors.bg};
+`;
 export default Trusted;
