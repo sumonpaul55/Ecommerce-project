1db923a18d61ea640d31e2af10cd0291f64c4028
sumon paul <sumpaul100@gmail.com>
2023-05-20 01:03:10 +0600

    trusted section done lesson9

--- src/components/Trusted.js
+++ src/components/Trusted.js
@@ -2,13 +2,40 @@ import React from "react";
 import styled from "styled-components";
 
 const Trusted = () => {
-  return <Wrapper>Trusted</Wrapper>;
+  return (
+    <Wrapper className="brand-section">
+      <div className="container">
+        <h3>Trusted by 1000+ Companies</h3>
+        <div className="brand-section-slider">
+          {/* slider 1st img */}
+          <div className="slider">
+            <img src="https://1000logos.net/wp-content/uploads/2021/11/Fiverr-Logo.png" alt="slider img" />
+          </div>
+          <div className="slider">
+            <img src="https://1000logos.net/wp-content/uploads/2021/11/Fiverr-Logo.png" alt="slider img" />
+          </div>
+          <div className="slider">
+            <img src="https://1000logos.net/wp-content/uploads/2021/11/Fiverr-Logo.png" alt="slider img" />
+          </div>
+          <div className="slider">
+            <img src="https://1000logos.net/wp-content/uploads/2021/11/Fiverr-Logo.png" alt="slider img" />
+          </div>
+          <div className="slider">
+            <img src="https://1000logos.net/wp-content/uploads/2021/11/Fiverr-Logo.png" alt="slider img" />
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.section`
   padding: 9rem 0;
   background-color: ${({ theme }) => theme.colors.bg};
   .brand-section {
     padding: 12rem 0 0 0;
+  }
+  img {
+    width: 10rem;
   }
   h3 {
     text-align: center;
