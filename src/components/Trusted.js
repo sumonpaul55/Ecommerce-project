c19e5a513c740cfb203644bd63adcbb4d600e0dc
sumon paul <sumpaul100@gmail.com>
2023-05-19 23:53:21 +0600

    trusted css added without media quary

--- src/components/Trusted.js
+++ src/components/Trusted.js
@@ -7,5 +7,24 @@ const Trusted = () => {
 const Wrapper = styled.section`
   padding: 9rem 0;
   background-color: ${({ theme }) => theme.colors.bg};
+  .brand-section {
+    padding: 12rem 0 0 0;
+  }
+  h3 {
+    text-align: center;
+    text-transform: capitalize;
+    color: ${({ theme }) => theme.colors.text};
+    font-size: 2rem;
+    font-weight: bold;
+  }
+  .brand-section-slider {
+    margin-top: 3.2rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+  }
+  @media screen and (max-width: ${({ theme }) => theme.media.mobile}) {
+  }
 `;
 export default Trusted;
