d8139e7afb2e5bcc1735990572fd8377c81c1923
sumon paul <sumpaul100@gmail.com>
2023-05-25 12:36:20 +0600

    google map added

--- src/Contact.js
+++ src/Contact.js
@@ -34,7 +34,21 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <h2>Contact</h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14493.20691640486!2d90.40411544999999!3d24.75084185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37564ee2ad25a80f%3A0x62cf0ccd4d0ff620!2sShashi%20Lodge!5e0!3m2!1sen!2sbd!4v1684996281393!5m2!1sen!2sbd"
+        width="100%"
+        height="450"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Shoshi lodge Mymensingh proper"
+      ></iframe>
+    </Wrapper>
+  );
 };
 
 export default Contact;
